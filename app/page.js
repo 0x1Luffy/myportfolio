c0371279104284ed81206ede28888ed7531be6b0
0x1Luffy <chetankesare06@gmail.com>
2023-08-29 23:04:15 +0530

    Initilized grid layout

--- app/page.js
+++ app/page.js
@@ -3,22 +3,7 @@ import React from 'react'
 
 const page = () => {
   return(
-    <>
-    <div id='wrapper' className='bg-black w-[100vw] h-[100vh] grid grid-cols-12 grid-rows-12 gap-7'>
-    <div className='bgColor'>1</div>
-    <div className='bgColor'>2</div>
-    <div className='bgColor'>3</div>
-    <div className='bgColor'>4</div>
-    <div className='bgColor'>5</div>
-    <div className='bgColor'>6</div>
-    <div className='bgColor'>7</div>
-    <div className='bgColor'>8</div>
-    <div className='bgColor'>9</div>
-    <div className='bgColor'>10</div>
-    <div className='bgColor'>11</div>
-    <div className='bgColor'>12</div>
-    </div>
-    </>
+    <div className='container'></div>
   )
   
 }
