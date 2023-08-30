600115db593b46a7092a0edbcd8bfb92c7a531bf
0x1Luffy <chetankesare06@gmail.com>
2023-08-31 00:56:08 +0530

    added colors and some styling done

--- Components/Rightside.js
+++ Components/Rightside.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Rightside = () => {
   return (
-    <div className='h-[990px] w-[900px] ml-10 mt-16 mb-16 bg-white rounded-lg'></div>
+    <div className=' mainContent h-[990px] w-[900px] ml-10 mt-16 mb-16 bg-white rounded-lg'></div>
   )
 }
 
