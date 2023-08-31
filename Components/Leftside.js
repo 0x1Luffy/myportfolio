c9c39ed28eddf611ec9963b0ecc1dd962f9a67d4
0x1Luffy <chetankesare06@gmail.com>
2023-08-31 11:46:18 +0530

    added name tag and taglinne, also added a wrapper.

--- Components/Leftside.js
+++ Components/Leftside.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Leftside = () => {
   return (
-    <div className=' leftContent flex justify-center h-[750px] w-[297px] mt-16 rounded-lg'>
-    <div className=' profilePic h-36 w-40 rounded-[35px] mt-28 '>
-
+    <div className=' leftContent flex justify-center  h-[750px] w-[297px] mt-16 rounded-lg'>
+    <div className='wrapper flex justify-center h-full w-full'>
+    <div className=' profilePic h-36 w-40 rounded-[65px] mt-20 absolute'>
+    </div>
+    <div className='myName h-10 w-full mt-64 text-center text-white font-bold text-2xl absolute'>CHETAN KESARE</div>
+    <div className='myTagline h-8 w-36 mt-[315px] rounded-[50px]  text-center pt-1 text-white font-medium text-opacity-75'>WEB DEVELOPER</div>
     </div>
     </div>
   )
