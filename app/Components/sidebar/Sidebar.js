277fa3ceae9359cd92dd6f972b01e563fe65063b
0x1Luffy <chetankesare06@gmail.com>
2023-09-15 03:53:31 +0530

    Created List For images and Icons usage.

--- app/Components/sidebar/Sidebar.js
+++ app/Components/sidebar/Sidebar.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
 import "./Sidebar.css";
+import Logo from "../../assests/logo.svg";
+
 const Sidebar = () => {
   return (
    <aside className='aside'>
-    <a href='$home' className='nav__logo'></a>
+    <a href='$home' className='nav__logo'>
+    <img src={Logo} alt='sideLogo' />
+    </a>
+    <nav className='nav'>
+      <div className='nav__menu'>
+      <ul className='nav__list'>
+        <li className='nav__item'>
+
+          <a href='#home' className='nav__link'>
+             <i className='icon-home'></i>
+          </a>
+        </li>
+
+        <li className='nav__item'>
+          <a href='' className='nav__link'></a>
+        </li>
+
+        <li className='nav__item'>
+          <a href='' className='nav__link'></a>
+        </li>
+
+        <li className='nav__item'>
+          <a href='' className='nav__link'></a>
+        </li>
+
+        <li className='nav__item'>
+          <a href='' className='nav__link'></a>
+        </li>
+
+
+      </ul>
+
+      </div>
+    </nav>
    </aside>
   )
 }
