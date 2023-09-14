7d62ae19f6b8a75b0c85d1a9d434ee1f1a4b45d0
0x1Luffy <chetankesare06@gmail.com>
2023-09-15 02:14:03 +0530

    changed the whole design. I got a new Idea!

--- Components/Rightside.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Rightside = () => {
-  return (
-    <div className=' mainContent h-[990px] w-[900px] ml-10 mt-16 mb-16 rounded-lg'></div>
-  )
-}
-
-export default Rightside
--- app/page.js
+++ app/page.js
@@ -1,15 +1,10 @@
 "use client" // Next.js works on both client and backend so we need to specify this we are using it on clint side only
-import Leftside from '@/Components/Leftside'
-import Rightside from '@/Components/Rightside'
 import React from 'react'
 
 
 const page = () => {
   return(
-    <div className='flex flex-row justify-center'>
-      <Leftside>
-      </Leftside>
-      <Rightside></Rightside>
+    <div>
       </div>
   )
   
