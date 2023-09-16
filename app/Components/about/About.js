10bf47d388ea6bcff2e11e74c9e0ede9ea30baa3
0x1Luffy <chetankesare06@gmail.com>
2023-09-16 17:51:49 +0530

    Started Designing Skills Section in about me.

--- app/Components/about/About.js
+++ app/Components/about/About.js
@@ -7,6 +7,77 @@ const About = () => {
       <h2 className='section__title'>
         About Me
       </h2>
+
+    <div className='about__container grid'>
+    <img src= "" alt='' className='about__img'/>
+
+    <div className='about__data grid'>
+      <div className='about__info'>
+        <p className='about__description'>
+          I am very enthusiastic of new technologies and looking for a
+          challenging and innovative career in the IT field, which will enable
+          me to utilize and enhance my skills in software development and UX
+          designing.
+
+
+
+          <a href='' className='btn'>Download Resume</a>
+        </p>
+      </div>
+
+      <div className='about__skills grid'>
+        <div className='skills__data'>
+          <div className='skills__titles'>
+            <h3 className='skills__name'>
+            Development
+            </h3>
+            <span className='skills__number'>90%</span>
+          </div>
+
+          <div className='skills__bar'>
+            <span className='skills__percentage'></span>
+          </div>
+        </div>
+
+        <div className='skills__data'>
+          <div className='skills__titles'>
+            <h3 className='skills__name'></h3>
+            <span className='skills__number'></span>
+          </div>
+
+          <div className='skills__bar'>
+            <span className='skills__percentage'></span>
+          </div>
+        </div>
+
+        <div className='skills__data'>
+          <div className='skills__titles'>
+            <h3 className='skills__name'></h3>
+            <span className='skills__number'></span>
+          </div>
+
+          <div className='skills__bar'>
+            <span className='skills__percentage'></span>
+          </div>
+        </div>
+
+        <div className='skills__data'>
+          <div className='skills__titles'>
+            <h3 className='skills__name'></h3>
+            <span className='skills__number'></span>
+          </div>
+
+          <div className='skills__bar'>
+            <span className='skills__percentage'></span>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div>
+    </div>
+
     </section>
   )
 }
