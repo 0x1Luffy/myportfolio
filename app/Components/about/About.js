db93f5cc3c6219b7a48f42f08a8355267b1b4266
0x1Luffy <chetankesare06@gmail.com>
2023-09-16 17:28:16 +0530

    Started Creating About Me Section.

--- app/Components/about/About.js
+++ app/Components/about/About.js
@@ -3,7 +3,11 @@ import "./About.css"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about container section' is='about'>
+      <h2 className='section__title'>
+        About Me
+      </h2>
+    </section>
   )
 }
 
