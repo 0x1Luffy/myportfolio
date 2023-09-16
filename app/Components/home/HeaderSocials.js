378cf7ffda33ab6c36eedb96e9e12e16bbbe5e8e
0x1Luffy <chetankesare06@gmail.com>
2023-09-16 16:27:02 +0530

    added profile photo and also added social links.

---
+++ app/Components/home/HeaderSocials.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+  <div className='home__socials'>
+        <a href='https://www.instagram.com/chetannn.k/' className='home__social-link' target='_blank'>
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href='https://www.behance.net/chetankesare1' className='home__social-link' target='_blank'>
+        <i class="fa-brands fa-behance"></i>
+        </a>
+
+        <a href='https://dribbble.com/chetankesare' className='home__social-link' target='_blank'>
+        <i class="fa-brands fa-dribbble"></i>
+        </a>
+
+        <a href='https://github.com/0x1Luffy' className='home__social-link' target='_blank'>
+        <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href='https://www.linkedin.com/in/chetan-kesare-057042195/' className='home__social-link' target='_blank'>
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+  </div>
+  )
+}
+
+export default HeaderSocials
--- app/Components/home/Home.js
+++ app/Components/home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import "./Home.css";
+import HeaderSocials from './HeaderSocials';
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <section className='home container' id='home'>
+   <div className='intro'>
+    <img src= "https://i.ibb.co/zS38BWV/9434621.png" height={145} width={160} alt='' className='home__img'/>
+    <h1 className='home__name'>Chetan Kesare</h1>
+    <span className='home__education'>
+    I'm a Full-Stack UX Designer
+    </span>
+
+    <HeaderSocials/>
+   </div>
+
+   </section>
   )
 }
 
 export default Home
+
