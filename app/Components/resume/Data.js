bc9d5e18a42f4de9bd02460cf0533f25ce93fa0c
0x1Luffy <chetankesare06@gmail.com>
2023-09-23 17:57:54 +0530

    Added experience and made some minor alignments.

--- app/Components/resume/Data.js
+++ app/Components/resume/Data.js
@@ -5,6 +5,7 @@ const Data =[
         icon:"icon-graduation",
         year: "2017-2018",
         title:"Class 10 CBSE",
+        desc:"70%",
     },
     {
         id:2,
@@ -12,6 +13,7 @@ const Data =[
         icon:"icon-graduation",
         year: "2018-2021",
         title:"Diploma In Computer Engineering",
+        desc:"91.20%",
     },
     {
         id:3,
@@ -19,6 +21,23 @@ const Data =[
         icon:"icon-graduation",
         year: "2021-2024",
         title:"B.E In Computer Engineering",
-    }
+        desc:"8.4 CGPA",
+    },
+    {
+        id:4,
+        category:"experience",
+        icon:"icon-briefcase",
+        year: "July-Augest-2023",
+        title:"CodeSoft",
+        desc:"UI/UX Design Intern at CodeSoft.",
+    },
+    {
+        id:4,
+        category:"experience",
+        icon:"icon-briefcase",
+        year: "June-July-2020",
+        title:"Domain Computer Education",
+        desc:"CISCO Router Switch Configuration and AWS Cloud Essentail.",
+    },
 ];
 export default Data
