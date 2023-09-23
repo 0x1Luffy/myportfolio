b24dedf2f5dd7add52e3d098b3ad7c1700b8a6ba
0x1Luffy <chetankesare06@gmail.com>
2023-09-23 20:51:10 +0530

    Defined a section for contact me.

--- app/Components/contact/Contact.js
+++ app/Components/contact/Contact.js
@@ -3,7 +3,28 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='contact container section' id='contact'>
+      <h2 className='section__title'>Get In Touch</h2>
+      <div className='contact__container grid'>
+        <div className='contact__info'>
+          <h3 className='contact__title'> Lets Talk About Everything!</h3>
+          <p className='contact__details'>Don't Like Forms? Send Me an Email. 👋</p>
+        </div>
+
+        <form action='' className='contact__form'>
+          <div className='contact__form-group'>
+            <div className='contact__form-div'>
+              <input type='text' className='contact__form-input' placeholder='Insert Your Name'/>
+            </div>
+            
+            <div className='contact__form-div'>
+              <input type='email' className='contact__form-input' placeholder='Insert Your Email'/>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </section>
   )
 }
 
