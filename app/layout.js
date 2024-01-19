d6f445c0eb3ad4fbc27c8ddbb4d001b1050d02ac
0x1Luffy <chetankesare06@gmail.com>
2024-01-19 15:07:16 +0530

    Added Web Analytics Feature of Vercel

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css"></link>
 
     </head>
-      <body>{children}
+      <body>
+      {children}
+      <Analytics />
       <script defer src="https://kit.fontawesome.com/34b8159489.js" crossorigin="anonymous"></script>
       </body>
     </html>
